74ee65a8770d56082c89d8d0447c8eec7dba9d45
Potfur <wachowski.michal@gmail.com>
2014-02-26 19:15:10 +0100

    cleanup

--- moss/storage/model/definition/relation/Relation.php
+++ moss/storage/model/definition/relation/Relation.php
@@ -7,40 +7,120 @@ use moss\storage\model\ModelInterface;
 
 class Relation implements RelationInterface
 {
-    private $entity;
-    private $type;
-    private $container;
-    private $keys = array();
-    private $local = array();
-    private $foreign = array();
+    protected $entity;
 
-    public function __construct($entity, $type, array $keys, $table = null)
+    protected $mediator;
+
+    protected $type;
+    protected $container;
+
+    protected $keys = array();
+    protected $in = array();
+    protected $out = array();
+
+    protected $local = array();
+    protected $foreign = array();
+
+    private $trough = false;
+
+    public function __construct($entity, $type, array $keys, $container = null, $mediator = null)
     {
-        if (!in_array($type, array(ModelInterface::RELATION_ONE, ModelInterface::RELATION_MANY))) {
-            throw new DefinitionException(sprintf('Invalid relation type %s in relation for %s', $type, $entity));
-        }
+        $this->entity = $entity ? ltrim($entity, '\\') : null;
 
-        $this->entity = '\\'.ltrim($entity, '\\');
+        $this->assertType($type);
+
         $this->type = $type;
-        $this->container = $table ? $table : substr($this->entity, strrpos($this->entity, '\\') + 1);
+        $this->container = $this->containerName($container);
 
         if (empty($keys)) {
             throw new DefinitionException(sprintf('No keys in "%s" relation definition', $this->entity));
         }
 
+        $this->trough = in_array($type, array(ModelInterface::RELATION_ONE_TROUGH, ModelInterface::RELATION_MANY_TROUGH));
+
+        if ($this->trough) {
+            if (empty($mediator)) {
+                throw new DefinitionException(sprintf('Missing mediator name for relation %s', $type, $this->entity));
+            }
+
+            $this->mediator = $mediator ? ltrim($mediator, '\\') : $mediator;
+
+            $this->assertTroughKeys($keys);
+            $this->assignKeys($keys[0], $this->in);
+            $this->assignKeys($keys[1], $this->out);
+            $this->keys = array_combine(array_keys($this->in), array_values($this->out));
+
+            return;
+        }
+
+        $this->assignKeys($keys, $this->keys);
+        $this->in = array_keys($this->keys);
+        $this->out = array_values($this->keys);
+    }
+
+    private function containerName($container)
+    {
+        if ($container) {
+            return $container;
+        }
+
+        $pos = strrpos($this->entity, '\\');
+        if ($pos === false) {
+            return $this->entity;
+        }
+
+        return substr($this->entity, strrpos($this->entity, '\\') + 1);
+    }
+
+    private function assignKeys(array $keys, array &$container)
+    {
         foreach ($keys as $local => $foreign) {
-            if (empty($local)) {
-                throw new DefinitionException(sprintf('Invalid local field name "%s" for "%s" relation definition', $local, $this->entity));
-            }
+            $this->assertField($local);
+            $this->assertField($foreign);
 
-            if (empty($foreign)) {
-                throw new DefinitionException(sprintf('Invalid foreign field name "%s" for "%s" relation definition', $foreign, $this->entity));
-            }
-
-            $this->keys[$local] = $foreign;
+            $container[$local] = $foreign;
         }
     }
 
+    protected function assertType($type)
+    {
+        $types = array(
+            ModelInterface::RELATION_ONE,
+            ModelInterface::RELATION_MANY,
+            ModelInterface::RELATION_ONE_TROUGH,
+            ModelInterface::RELATION_MANY_TROUGH
+        );
+
+        if (!in_array($type, $types)) {
+            throw new DefinitionException(sprintf('Invalid relation type %s in relation for %s', $type, $this->entity));
+        }
+    }
+
+    protected function assertTroughKeys($keys)
+    {
+        if (!isset($keys[0], $keys[1])) {
+            throw new DefinitionException(sprintf('Invalid keys for relation "%s", must have two arrays, got %u', $this->entity, count($keys)));
+        }
+
+        if (count($keys[0]) !== count($keys[1])) {
+            throw new DefinitionException(sprintf('Both key arrays for relation "%s", must same number of elements', $this->entity));
+        }
+    }
+
+    protected function assertField($field)
+    {
+        if (!is_string($field)) {
+            throw new DefinitionException(sprintf('Field name for relation "%s.%s" must be string, %s given', $this->entity, $field, gettype($field)));
+        }
+
+        if (empty($field)) {
+            throw new DefinitionException(sprintf('Field name for "%s.%s" can not be empty', $this->entity, $field));
+        }
+
+        if (is_numeric($field)) {
+            throw new DefinitionException(sprintf('Field name for "%s.%s" can not be numeric', $this->entity, $field));
+        }
+    }
 
     /**
      * Returns relation name in entity
@@ -50,6 +130,16 @@ class Relation implements RelationInterface
     public function name()
     {
         return $this->container();
+    }
+
+    /**
+     * Returns relation mediating instance
+     *
+     * @return string
+     */
+    public function mediator()
+    {
+        return $this->mediator;
     }
 
     /**
@@ -93,6 +183,26 @@ class Relation implements RelationInterface
     }
 
     /**
+     * Returns array containing local keys
+     *
+     * @return array
+     */
+    public function localKeys()
+    {
+        return $this->in;
+    }
+
+    /**
+     * Returns array containing foreign keys
+     *
+     * @return array
+     */
+    public function foreignKeys()
+    {
+        return $this->out;
+    }
+
+    /**
      * Returns associative array containing local key - value pairs
      *
      * @param array $localValues ;
@@ -104,9 +214,7 @@ class Relation implements RelationInterface
     {
         if ($localValues !== array()) {
             foreach ($localValues as $field => $value) {
-                if (empty($field)) {
-                    throw new DefinitionException(sprintf('Invalid local field name "%s" in "%s" relation', $field, $this->entity));
-                }
+                $this->assertField($field);
 
                 $this->local[$field] = $value;
             }
@@ -127,9 +235,7 @@ class Relation implements RelationInterface
     {
         if ($foreignValues !== array()) {
             foreach ($foreignValues as $field => $value) {
-                if (empty($field)) {
-                    throw new DefinitionException(sprintf('Invalid foreign field name "%s" in "%s" relation', $field, $this->entity));
-                }
+                $this->assertField($field);
 
                 $this->foreign[$field] = $value;
             }
