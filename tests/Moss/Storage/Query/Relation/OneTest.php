07e9e56c2344f0a3f8137136501e9dac8402bac2
Potfur <wachowski.michal@gmail.com>
2014-03-04 20:58:33 +0100

    blank relation tests

--- tests/Moss/Storage/Query/Relation/OneTest.php
+++ tests/Moss/Storage/Query/Relation/OneTest.php
@@ -12,13 +12,7 @@ class OneTest extends \PHPUnit_Framework_TestCase
 
     public function testRead()
     {
-        $bag = $this->mockModelBag();
-        $rel = new One(
-            $this->mockQuery(),
-            $bag->get('table')
-                ->relation('other'),
-            $bag
-        );
+        $this->markTestIncomplete();
     }
 
     public function testWrite()
@@ -34,55 +28,5 @@ class OneTest extends \PHPUnit_Framework_TestCase
     public function testClear()
     {
         $this->markTestIncomplete();
-    }
-
-    protected function mockQuery()
-    {
-        $mock = $this->getMock('\Moss\Storage\Query\QueryInterface');
-
-        return $mock;
-    }
-
-    protected function mockRelation(array $keys)
-    {
-        $mock = $this->getMock('\Moss\Storage\Model\Definition\Relation\RelationInterface');
-
-        return $mock;
-    }
-
-    protected function mockModelBag($keys = array('id' => 'id'))
-    {
-        $table = new Model(
-            '\stdClass',
-            'table',
-            array(
-                new Field('id', Model::FIELD_INTEGER, array('unsigned', 'auto_increment')),
-                new Field('text', Model::FIELD_STRING, array('length' => '128', 'null')),
-            ),
-            array(
-                new Primary(array('id')),
-            ),
-            array(
-                $this->mockRelation($keys)
-            )
-        );
-
-        $other = new Model(
-            '\altClass',
-            'other',
-            array(
-                new Field('id', Model::FIELD_INTEGER, array('unsigned', 'auto_increment')),
-                new Field('text', Model::FIELD_STRING, array('length' => '128', 'null')),
-            ),
-            array(
-                new Primary(array('id')),
-            )
-        );
-
-        $bag = new ModelBag();
-        $bag->set($table, 'table');
-        $bag->set($other, 'other');
-
-        return $bag;
     }
 }
