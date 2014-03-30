4dfa08595e32fa1960b5a39d7462b2d67b4335d1
Potfur <wachowski.michal@gmail.com>
2014-03-30 10:34:27 +0200

    separated Query and Schema

--- Moss/Storage/Storage.php
+++ Moss/Storage/Storage.php
@@ -11,65 +11,26 @@
 
 namespace Moss\Storage;
 
-use Moss\Storage\Builder\BuilderInterface;
-use Moss\Storage\Builder\QueryInterface;
-use Moss\Storage\Builder\SchemaInterface;
 use Moss\Storage\Driver\DriverInterface;
 use Moss\Storage\Model\ModelBag;
 use Moss\Storage\Model\ModelInterface;
-use Moss\Storage\Query\Query;
-use Moss\Storage\Query\Schema;
 
 /**
- * Facade that eases calling queries
+ * Abstract class implementing basic storage functionality
  *
  * @author  Michal Wachowski <wachowski.michal@gmail.com>
  * @package Moss\Storage
  */
-class Storage implements StorageInterface
+abstract class Storage implements StorageInterface
 {
 
     /** @var DriverInterface */
     protected $driver;
 
-    /** @var BuilderInterface */
-    protected $builders = array(
-        'query' => null,
-        'schema' => null
-    );
-
     /** @var ModelBag */
-    public $models = array();
+    protected $bag;
 
-    /**
-     * Constructor
-     *
-     * @param DriverInterface    $driver
-     * @param BuilderInterface[] $builders
-     *
-     * @throws StorageException
-     */
-    public function __construct(DriverInterface $driver, array $builders)
-    {
-        $this->driver = & $driver;
-        $this->models = new ModelBag();
-
-        foreach ($builders as $builder) {
-            if (!$builder instanceof BuilderInterface) {
-                throw new StorageException('Builder must be an instance of BuilderInterface');
-            }
-
-            if ($builder instanceof QueryInterface) {
-                $this->builders['query'] = & $builder;
-            }
-
-            if ($builder instanceof SchemaInterface) {
-                $this->builders['schema'] = & $builder;
-            }
-
-            unset($builder);
-        }
-    }
+    protected $instance;
 
     /**
      * Returns adapter instance for specified entity
@@ -91,7 +52,7 @@ class Storage implements StorageInterface
      */
     public function register(ModelInterface $model, $alias = null)
     {
-        $this->models->set($model, $alias);
+        $this->bag->set($model, $alias);
 
         return $this;
     }
@@ -103,193 +64,7 @@ class Storage implements StorageInterface
      */
     public function models()
     {
-        return $this->models;
-    }
-
-    /**
-     * Returns true if entity table exists
-     *
-     * @param string|object $entity
-     *
-     * @return Schema
-     */
-    public function check($entity = null)
-    {
-        return $this->schema()
-            ->check($entity);
-    }
-
-    /**
-     * Returns query creating entity table
-     *
-     * @param string|object $entity
-     *
-     * @return Schema
-     */
-    public function create($entity = null)
-    {
-        return $this->schema()
-            ->create($entity);
-    }
-
-    /**
-     * Returns query altering entity table
-     *
-     * @param string|object $entity
-     *
-     * @return Schema
-     */
-    public function alter($entity = null)
-    {
-        return $this->schema()
-            ->alter($entity);
-    }
-
-    /**
-     * Returns query removing entity table
-     *
-     * @param string|object $entity
-     *
-     * @return Schema
-     */
-    public function drop($entity = null)
-    {
-        return $this->schema()
-            ->drop($entity);
-    }
-
-    /**
-     * @return Schema
-     * @throws StorageException
-     */
-    protected function schema()
-    {
-        if (!$this->builders['schema']) {
-            throw new StorageException('Unable to create schema, missing schema builder');
-        }
-
-        $schema = new Schema($this->driver, $this->builders['schema'], $this->models);
-
-        return $schema;
-    }
-
-    /**
-     * Returns count query for passed entity class
-     *
-     * @param string $entity
-     *
-     * @return Query
-     */
-    public function num($entity)
-    {
-        return $this->query()
-            ->num($entity);
-    }
-
-    /**
-     * Returns read single entity for passed entity class
-     *
-     * @param string $entity
-     *
-     * @return Query
-     */
-    public function readOne($entity)
-    {
-        return $this->query()
-            ->readOne($entity);
-    }
-
-    /**
-     * Returns read query for passed entity class
-     *
-     * @param string $entity
-     *
-     * @return Query
-     */
-    public function read($entity)
-    {
-        return $this->query()
-            ->read($entity);
-    }
-
-    /**
-     * Returns write query for passed entity object or entity class
-     *
-     * @param string|object $instance
-     *
-     * @return Query
-     */
-    public function write($instance)
-    {
-        return $this->query()
-            ->write(get_class($instance), $instance);
-    }
-
-    /**
-     * Returns insert query for passed entity object or entity class
-     *
-     * @param string|object $instance
-     *
-     * @return Query
-     */
-    public function insert($instance)
-    {
-        return $this->query()
-            ->insert(get_class($instance), $instance);
-    }
-
-    /**
-     * Returns update query for passed entity object or entity class
-     *
-     * @param string|object $instance
-     *
-     * @return Query
-     */
-    public function update($instance)
-    {
-        return $this->query()
-            ->update(get_class($instance), $instance);
-    }
-
-    /**
-     * Returns delete query for passed entity object or entity class
-     *
-     * @param string|object $instance
-     *
-     * @return Query
-     */
-    public function delete($instance)
-    {
-        return $this->query()
-            ->delete(get_class($instance), $instance);
-    }
-
-    /**
-     * Returns clear query for passed entity class
-     *
-     * @param string $entity
-     *
-     * @return Query
-     */
-    public function clear($entity)
-    {
-        return $this->query()
-            ->clear($entity);
-    }
-
-    /**
-     * @return Query
-     * @throws StorageException
-     */
-    protected function query()
-    {
-        if (!$this->builders['query']) {
-            throw new StorageException('Unable to create query, missing query builder');
-        }
-
-        $query = new Query($this->driver, $this->builders['query'], $this->models);
-
-        return $query;
+        return $this->bag;
     }
 
     /**
---
+++ Moss/Storage/StorageQuery.php
@@ -0,0 +1,151 @@
+<?php
+
+/*
+ * This file is part of the Storage package
+ *
+ * (c) Michal Wachowski <wachowski.michal@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Moss\Storage;
+
+use Moss\Storage\Builder\QueryBuilderInterface;
+use Moss\Storage\Driver\DriverInterface;
+use Moss\Storage\Model\ModelBag;
+use Moss\Storage\Query\Query;
+
+/**
+ * Facade that eases calling queries
+ *
+ * @author  Michal Wachowski <wachowski.michal@gmail.com>
+ * @package Moss\Storage
+ */
+class StorageQuery extends Storage
+{
+    /**
+     * Constructor
+     *
+     * @param DriverInterface       $driver
+     * @param QueryBuilderInterface $builder
+     */
+    public function __construct(DriverInterface $driver, QueryBuilderInterface $builder)
+    {
+        $this->driver = & $driver;
+        $this->bag = new ModelBag();
+        $this->instance = new Query($this->driver, $builder, $this->bag);
+    }
+
+    /**
+     * Returns count query for passed entity class
+     *
+     * @param string $entity
+     *
+     * @return Query
+     */
+    public function num($entity)
+    {
+        return $this->query()
+            ->num($entity);
+    }
+
+    /**
+     * Returns read single entity for passed entity class
+     *
+     * @param string $entity
+     *
+     * @return Query
+     */
+    public function readOne($entity)
+    {
+        return $this->query()
+            ->readOne($entity);
+    }
+
+    /**
+     * Returns read query for passed entity class
+     *
+     * @param string $entity
+     *
+     * @return Query
+     */
+    public function read($entity)
+    {
+        return $this->query()
+            ->read($entity);
+    }
+
+    /**
+     * Returns write query for passed entity object or entity class
+     *
+     * @param string|object $instance
+     *
+     * @return Query
+     */
+    public function write($instance)
+    {
+        return $this->query()
+            ->write(get_class($instance), $instance);
+    }
+
+    /**
+     * Returns insert query for passed entity object or entity class
+     *
+     * @param string|object $instance
+     *
+     * @return Query
+     */
+    public function insert($instance)
+    {
+        return $this->query()
+            ->insert(get_class($instance), $instance);
+    }
+
+    /**
+     * Returns update query for passed entity object or entity class
+     *
+     * @param string|object $instance
+     *
+     * @return Query
+     */
+    public function update($instance)
+    {
+        return $this->query()
+            ->update(get_class($instance), $instance);
+    }
+
+    /**
+     * Returns delete query for passed entity object or entity class
+     *
+     * @param string|object $instance
+     *
+     * @return Query
+     */
+    public function delete($instance)
+    {
+        return $this->query()
+            ->delete(get_class($instance), $instance);
+    }
+
+    /**
+     * Returns clear query for passed entity class
+     *
+     * @param string $entity
+     *
+     * @return Query
+     */
+    public function clear($entity)
+    {
+        return $this->query()
+            ->clear($entity);
+    }
+
+    /**
+     * @return Query
+     */
+    private function query()
+    {
+        return clone $this->instance;
+    }
+}
